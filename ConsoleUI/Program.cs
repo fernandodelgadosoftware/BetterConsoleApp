e4c3cd4f1a375aa99c61c2fdd0787d7c2bbcf7df
fernandodelgadosoftware <fernandodelgadosoftware@gmail.com>
2023-08-21 12:59:26 -0500

    first

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Project Goals: Use DI, Serilog and Settings
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+static void BuildConfig(IConfigurationBuilder configurationBuilder)
+{
+    configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+        .AddEnvironmentVariables();
+}
